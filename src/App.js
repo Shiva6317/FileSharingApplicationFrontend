2c8dff2512212567c05934841e6b117854e51d78
Sanjay Mishra <shivjeetpaswan86@gmail.com>
2023-12-04 05:47:34 +0530

    handle a tag error

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ function App() {
 
           />
 
-         <a className="a-tag" href={fileurl}>pppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp</a>
+         <a className="a-tag" href={fileurl}>{fileurl}</a>
 
         </div>
 
